85a3ed0a824014b6f065c70fe434b86eeafd8729
Dipak <Dipak@DIPAK-PC>
2018-03-10 13:30:44 +0530

    Location Model class 1st change

---
+++ src/main/java/com/app/model/Location.java
@@ -0,0 +1,106 @@
+package com.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+/**
+ * @author Dipak
+ *
+ */
+@Entity
+@Table(name="loc_tab1")
+public class Location {
+	@Id
+	@Column(name="locId", length=10)
+	@GenericGenerator(name = "sequence_loc_id", strategy = "com.app.util.LocationIdGenerator")
+	@GeneratedValue(generator = "sequence_loc_id")  
+	private String locId;
+	private String locName;
+	private String locType;
+	private String pincode;
+	private String shippingType;
+	private String contactDetails;
+	private String processCode;
+	private String localTransport;
+	public String getLocId() {
+		return locId;
+	}
+	public void setLocId(String locId) {
+		this.locId = locId;
+	}
+	public String getLocName() {
+		return locName;
+	}
+	public void setLocName(String locName) {
+		this.locName = locName;
+	}
+	public String getLocType() {
+		return locType;
+	}
+	public void setLocType(String locType) {
+		this.locType = locType;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getShippingType() {
+		return shippingType;
+	}
+	public void setShippingType(String shippingType) {
+		this.shippingType = shippingType;
+	}
+	public String getContactDetails() {
+		return contactDetails;
+	}
+	public void setContactDetails(String contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+	public String getProcessCode() {
+		return processCode;
+	}
+	public void setProcessCode(String processCode) {
+		this.processCode = processCode;
+	}
+	public String getLocalTransport() {
+		return localTransport;
+	}
+	public void setLocalTransport(String localTransport) {
+		this.localTransport = localTransport;
+	}
+	public Location(String locId, String locName, String locType, String pincode,
+			String shippingType, String contactDetails, String processCode,
+			String localTransport) {
+		super();
+		this.locId = locId;
+		this.locName = locName;
+		this.locType = locType;
+		this.pincode = pincode;
+		this.shippingType = shippingType;
+		this.contactDetails = contactDetails;
+		this.processCode = processCode;
+		this.localTransport = localTransport;
+	}
+	public Location(String locId) {
+		super();
+		this.locId = locId;
+	}
+	@Override
+	public String toString() {
+		return "Location [locId=" + locId + ", locName=" + locName
+				+ ", locType=" + locType + ", pincode=" + pincode
+				+ ", shippingType=" + shippingType + ", contactDetails="
+				+ contactDetails + ", processCode=" + processCode
+				+ ", localTransport=" + localTransport + "]";
+	}
+	
+
+}
